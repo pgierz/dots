5a19aceba77cfb4e1274ad9917a1d927fad58d93
Paul Gierz <a270077@mlogin108.hpc.dkrz.de>
2020-12-31 18:39:44 +0100

    spring cleaning

---
+++ .config/fzf/fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */pf/a/a270077/.local/src/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/pf/a/a270077/.local/src/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/pf/a/a270077/.local/src/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/pf/a/a270077/.local/src/fzf/shell/key-bindings.zsh"
