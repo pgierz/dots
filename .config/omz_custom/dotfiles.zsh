0d0c8471038615fc6c9c9ea2a0e79bad67560a57
Paul Gierz <pgierz@awi.de>
2021-01-15 08:57:52 +0100

    feat: bat, gitlab_cli, dotfiles

--- .config/omz_custom/dotfiles.zsh
+++ .config/omz_custom/dotfiles.zsh
@@ -1,1 +1,1 @@
-alias config='/usr/bin/git --git-dir=/pf/a/a270077/.cfg/ --work-tree=/pf/a/a270077'
+alias config="/usr/bin/git --git-dir=${HOME}/.cfg/ --work-tree=${HOME}"
---
+++ .config/omz_custom/gitlab_cli.zsh
@@ -0,0 +1,2 @@
+export PYTHON_GITLAB_CFG="${XDG_CONFIG_HOME:=${HOME}/.config}"/python-gitlab/config.ini
+eval "$(register-python-argcomplete gitlab)"
